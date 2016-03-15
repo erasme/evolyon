3af34380dd785dbfcf7492287f1bb844b10ff988
Clément Renaud <clement.renaud@gmail.com>
2016-03-15 17:34:21 +0100

    init routes

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ var port = process.env.PORT || 3000;
 var config = require("./config");
 
 
+app.get('/', function(req, res){
+  res.send('Yo !');
+})
+
+app.get('/touchscreen', function(req, res){
+  res.send('Touchscreen');
+})
+
+app.get('/videoproj', function(req, res){
+  res.send('Videoproj');
+})
+
+app.get('/single/:id', function(req,res){
+  res.send('cell ' + req.params.id);
+});
+
+server.listen( port, function( ) {
+    console.log( 'Server listening at port %d', port );
+});
+
 app.use( express.static( __dirname + '/public' ) );
 
 io.on( 'connection', function( socket ) {
