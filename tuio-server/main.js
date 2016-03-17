39cf0d70beb512ac3d2d98b5f572d58e72033975
Clément Renaud <clement.renaud@gmail.com>
2016-03-17 16:52:40 +0100

    tuio

---
+++ tuio-server/main.js
@@ -0,0 +1,40 @@
+var client = new Tuio.Client({
+    host: "http://localhost:5000"
+}),
+
+onAddTuioCursor = function(addCursor) {
+  console.log(addCursor);
+},
+
+onUpdateTuioCursor = function(updateCursor) {
+  console.log(updateCursor);
+},
+
+onRemoveTuioCursor = function(removeCursor) {
+  console.log(removeCursor);
+},
+
+onAddTuioObject = function(addObject) {
+    console.log(addObject);
+},
+
+onUpdateTuioObject = function(updateObject) {
+    console.log(updateObject);
+},
+
+onRemoveTuioObject = function(removeObject) {
+    console.log(removeObject);
+},
+
+onRefresh = function(time) {
+  console.log(time);
+};
+
+client.on("addTuioCursor", onAddTuioCursor);
+client.on("updateTuioCursor", onUpdateTuioCursor);
+client.on("removeTuioCursor", onRemoveTuioCursor);
+client.on("addTuioObject", onAddTuioObject);
+client.on("updateTuioObject", onUpdateTuioObject);
+client.on("removeTuioObject", onRemoveTuioObject);
+client.on("refresh", onRefresh);
+client.connect();
