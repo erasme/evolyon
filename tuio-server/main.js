a2eb355764db0a086ec462b05eb1bf6e79a96f37
Clément Renaud <clement.renaud@gmail.com>
2016-03-17 17:21:19 +0100

    tuio

--- tuio-server/main.js
+++ tuio-server/main.js
@@ -1,40 +1,7 @@
-var client = new Tuio.Client({
-    host: "http://localhost:5000"
-}),
+var osc = require('node-osc');
 
-onAddTuioCursor = function(addCursor) {
-  console.log(addCursor);
-},
+var oscServer = new osc.Server(3333, '0.0.0.0');
 
-onUpdateTuioCursor = function(updateCursor) {
-  console.log(updateCursor);
-},
-
-onRemoveTuioCursor = function(removeCursor) {
-  console.log(removeCursor);
-},
-
-onAddTuioObject = function(addObject) {
-    console.log(addObject);
-},
-
-onUpdateTuioObject = function(updateObject) {
-    console.log(updateObject);
-},
-
-onRemoveTuioObject = function(removeObject) {
-    console.log(removeObject);
-},
-
-onRefresh = function(time) {
-  console.log(time);
-};
-
-client.on("addTuioCursor", onAddTuioCursor);
-client.on("updateTuioCursor", onUpdateTuioCursor);
-client.on("removeTuioCursor", onRemoveTuioCursor);
-client.on("addTuioObject", onAddTuioObject);
-client.on("updateTuioObject", onUpdateTuioObject);
-client.on("removeTuioObject", onRemoveTuioObject);
-client.on("refresh", onRefresh);
-client.connect();
+oscServer.on("message", function (msg, rinfo) {
+  console.log("TUIO message:"); console.log(msg);
+});
