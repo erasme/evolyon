b38a4a87df3de05b04daa35ffffdc71c966fdb0e
Clément Renaud <clement.renaud@gmail.com>
2016-03-18 10:10:18 +0100

    test graphics

--- public/main.js
+++ public/main.js
@@ -9,22 +9,43 @@ $( function() {
 			console.log(data);
 		});
 
-		var circle = d3.select("#visual")
+
+
+		var svg = d3.select("#visual")
 			.append("svg")
 			.attr("width", "100%")
 			.attr("height", window.innerHeight)
-			.append("circle")
-			.attr("cx", 200)
-			.attr("cy", 200)
-			.attr("r", 100)
-			.style("fill", "red")
+		// 	.append("circle")
+		// 	.attr("cx", 200)
+		// 	.attr("cy", 200)
+		// 	.attr("r", 100)
+		// 	.style("fill", "red")
+
+		var i = 0;
 
 		socket.on( 'gesture', function(data){
 			// console.log(data);
-			circle
-				.attr("cx", data.x+"%")
-				.attr("cy", data.y+"%")
-				.attr("r", data.y/3+"%")
+
+			  // var m = d3.mouse(this);
+
+			  svg.insert("circle", "rect")
+			      .attr("cx", data.x+"%")
+			      .attr("cy", 100-data.y+"%")
+			      .attr("r", data.z/12+"%")
+			      .style("stroke", d3.hsl((i = (i + 1) % 360), 1, .5))
+			      .style("stroke-opacity", 1)
+			    .transition()
+			      .duration(2000)
+			      .ease(Math.sqrt)
+			      .attr("r", 100)
+			      .style("stroke-opacity", 1e-6)
+			      .remove();
+
+
+			// circle
+			// 	.attr("cx", data.x+"%")
+			// 	.attr("cy", 100-data.y+"%")
+			// 	.attr("r", data.z/12+"%")
 
 
 		});
