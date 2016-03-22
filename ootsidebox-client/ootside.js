133437e80b6d46f3792779ff323e20a0468d399f
Makio135 <makio135@gmail.com>
2016-03-22 17:36:45 +0100

    creating phoneReady event

--- ootsidebox-client/ootside.js
+++ ootsidebox-client/ootside.js
@@ -109,7 +109,8 @@ var POS_LENGTH = 20,
     },
     prevTime = Date.now(),
     prevEvent = null,
-    minTimeDiffBetweenEvents = 20;
+    minTimeDiffBetweenEvents = 20,
+    phoneReady = false;
 
 
 // tell OootsideBox to send raw values instead of pre-calculated events
@@ -200,8 +201,15 @@ ootsidebox.on( 'data', function( data, err ) {
     updateMinMax( gesture );
 
     var presence = ( gesture.z < 200 );
+    
+    if( presence && phoneReady && Date.now() - timePresStarted > 3000 ){
+    	//send Cell to Phone
+    	io.emit( 'phoneReady', {} );
+    	phoneReady = false;
+    }
+
     // console.log( presence );
-    if( !prevPresence && presence){
+    if( !prevPresence && presence ){
     	timePresStarted = Date.now();
         io.emit('presence', { 'presence': true });
         console.log('presence', { 'presence': true });
@@ -246,6 +254,11 @@ ootsidebox.on( 'data', function( data, err ) {
     prevGest = gesture;
 } );
 
+/*
+on('phoneReady', function(){
+	phoneReady = true;
+});
+*/
 
 /*
         __  ___      __  __       ____                 __  _
