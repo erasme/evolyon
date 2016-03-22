8efc479c3dc1a5b467aa6eef240408e44eef4b2b
Clément Renaud <clement.renaud@gmail.com>
2016-03-22 19:58:35 +0100

    init particle

--- public/js/Shapes_3.js
+++ public/js/Shapes_3.js
@@ -2,7 +2,7 @@ var isWeb = false;
 var triangle;
 
 var threshold = 30;
-var accChangeX = 0; 
+var accChangeX = 0;
 var accChangeY = 0;
 var accChangeT = 0;
 
@@ -10,48 +10,52 @@ var backgroundOpacity = 0;
 var bounceSpeed = 0;
 
 var nomnoms = [];
-var mic;
+// var mic;
 
+var isLoaded = false;
 
 function setup() {
   var canvas = createCanvas(window.innerWidth, window.innerHeight);
   canvas.parent('particle');
-  triangle = new Triangle();
-  
+  triangle = new Triangle(4);
+
   for (var i = 0; i <= 5; i++) {
     newNomNom();
   }
-  
-  mic = new p5.AudioIn();
-  mic.start();
-  
-//  size(window.innerWidth, window.innerHeight, P3D);
+
   smooth();
+}
+
+function start() {
+  isLoaded = true;
 }
 
 function draw(event) {
   background('rgb(36,31,56)');
-  
-  fill('rgb(255,0,0)');
-  micLevel = mic.getLevel();
-  ellipse(width/2, constrain(height-micLevel*height*5, 0, height), 10, 10);
 
-  
+  fill('rgb(255,0,0)');
+  // micLevel = mic.getLevel();
+  // ellipse(width/2, constrain(height-micLevel*height*5, 0, height), 10, 10);
+
+
   if (backgroundOpacity > .1) {
     fill('rgba(255,33,124,'+backgroundOpacity+')');
     rect(0,0,width,height);
   }
-  
-  canvasUpdate();
-  
-  for (var i = 0; i < nomnoms.length; i++) {
-    nomnoms[i].update();
-    nomnoms[i].draw();
+
+  if(isLoaded) {
+
+    canvasUpdate();
+
+    for (var i = 0; i < nomnoms.length; i++) {
+      nomnoms[i].update();
+      nomnoms[i].draw();
+    }
+
+    triangle.move();
+    triangle.draw();
   }
-  
-  triangle.move();
-  triangle.draw();
-  
+
 //  checkForShake();
 }
 
@@ -105,12 +109,12 @@ function checkForShake() {
   if (accChangeT >= threshold) {
     triangle.shake();
     triangle.turn();
-  } 
+  }
   // If not shake
   else {
     triangle.stopShake();
     triangle.turn();
-    triangle.move(); 
+    triangle.move();
   }
 }
 */
@@ -136,4 +140,3 @@ function polygon(centreX, centreY, nbCotes, radius, angle, amplitude) {
   }
   endShape(CLOSE);
 }
-
--- public/js/Triangle.js
+++ public/js/Triangle.js
@@ -1,13 +1,13 @@
-function Triangle() {
+function Triangle(_nbCotes) {
 
   // Speed - Velocity
   this.vx = 0;
   this.vy = 0;
-   
+
   // Acceleration
   this.ax = 0;
   this.ay = 0;
-   
+
   this.vMultiplier = 0.007;
   this.bMultiplier = 0.8;
 
@@ -22,51 +22,54 @@ function Triangle() {
   this.rotate = 0;
   this.scale = 1;
 
-  
+
+  this.nbCotes = _nbCotes
+
+
   this.update = function(event) {
     this.x = touchX;
     this.y = touchY;
   }
-  
+
   this.move = function() {
-    
+
     this.rotate += this.vx/2000;
-    
+
     if (this.rotate >= 360) {
       this.rotate = 0;
     }
-    
+
   //  this.x += this.xspeed * this.direction;
-  //  this.y += this.yspeed * this.direction;  
-    
+  //  this.y += this.yspeed * this.direction;
+
     this.ax = rotationX;
   	this.ay = rotationY;
-  
+
   	this.vx = this.vx + this.ay + accelerationY*2;
   	this.vy = this.vy + this.ax + accelerationX*2;
-  	
-  	this.y = this.y + this.vy * this.vMultiplier; 
+
+  	this.y = this.y + this.vy * this.vMultiplier;
   	this.x = this.x + this.vx * this.vMultiplier;
-  
+
   	// Bounce when touch the edge of the canvas
-  	if (this.x < 0) { 
-  		this.x = 0; 
-  		this.vx = -this.vx * this.bMultiplier; 
+  	if (this.x < 0) {
+  		this.x = 0;
+  		this.vx = -this.vx * this.bMultiplier;
   		setBounceEffect();
   	}
-   	if (this.y < 0) { 
-   		this.y = 0; 
-   		this.vy = -this.vy * this.bMultiplier; 
+   	if (this.y < 0) {
+   		this.y = 0;
+   		this.vy = -this.vy * this.bMultiplier;
   		setBounceEffect();
    	}
-   	if (this.x > width - 20) { 
-   		this.x = width - 20; 
-   		this.vx = -this.vx * this.bMultiplier; 
+   	if (this.x > width - 20) {
+   		this.x = width - 20;
+   		this.vx = -this.vx * this.bMultiplier;
   		setBounceEffect();
    	}
-   	if (this.y > height - 20) { 
-   		this.y = height - 20; 
-   		this.vy = -this.vy * this.bMultiplier; 
+   	if (this.y > height - 20) {
+   		this.y = height - 20;
+   		this.vy = -this.vy * this.bMultiplier;
   		setBounceEffect();
    	}
   }
@@ -85,36 +88,36 @@ function Triangle() {
 
     stroke('rgb(255,33,124)');
     strokeWeight(3);
-    polygon(random(-1, 1), random(-1, 1), 3, 50, -90, (sin(frameCount*bounceSpeed/60.)*0.2+1) );
+    polygon(random(-1, 1), random(-1, 1), this.nbCotes, 50, -90, (sin(frameCount*bounceSpeed/60.)*0.2+1) );
     stroke('rgba(255,33,124,.3)');
     strokeWeight(10);
-    polygon(random(-1, 1), random(-1, 1), 3, 50, -90, sin(frameCount*bounceSpeed/60.)*0.2+1);
+    polygon(random(-1, 1), random(-1, 1), this.nbCotes, 50, -90, sin(frameCount*bounceSpeed/60.)*0.2+1);
     stroke('rgba(255,33,124,.2)');
     strokeWeight(20);
-    polygon(random(-1, 1), random(-1, 1), 3, 50, -90, sin(frameCount*bounceSpeed/60.)*0.2+1);
+    polygon(random(-1, 1), random(-1, 1), this.nbCotes, 50, -90, sin(frameCount*bounceSpeed/60.)*0.2+1);
 
     pop();
   }
-  
-  // Bounce when touch the edge of the canvas  
+
+  // Bounce when touch the edge of the canvas
   /*
   this.turn = function() {
-    if (this.x < 0) { 
-      this.x = 0; 
-      this.direction = -this.direction; 
+    if (this.x < 0) {
+      this.x = 0;
+      this.direction = -this.direction;
     }
-    else if (this.y < 0) { 
-      this.y = 0; 
-      this.direction = -this.direction;   
+    else if (this.y < 0) {
+      this.y = 0;
+      this.direction = -this.direction;
     }
-    else if (this.x > width - 20) { 
-      this.x = width - 20; 
-      this.direction = -this.direction; 
+    else if (this.x > width - 20) {
+      this.x = width - 20;
+      this.direction = -this.direction;
     }
-    else if (this.y > height - 20) { 
-      this.y = height - 20; 
-      this.direction = -this.direction;   
-    } 
+    else if (this.y > height - 20) {
+      this.y = height - 20;
+      this.direction = -this.direction;
+    }
   }
   */
   /*
@@ -122,20 +125,19 @@ function Triangle() {
     this.xspeed += random(5, accChangeX/6);
     this.yspeed += random(5, accChangeX/6);
   }
-  
+
   this.stopShake = function() {
     if (this.xspeed > this.oxspeed) {
       this.xspeed -= 0.6;
-    } 
+    }
     else {
       this.xspeed = this.oxspeed;
     }
     if (this.yspeed > this.oyspeed) {
       this.yspeed -= 0.6;
-    } 
+    }
     else {
       this.yspeed = this.oyspeed;
     }
   }*/
-} 
-
+}
